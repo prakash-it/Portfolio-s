cacefcb7b5442ac1179217102a54c3df35a382c1
prakash-it <prakashkandasamy4@gmail.com>
2024-07-12 08:27:49 +0530

    Portfolio

---
+++ Portfolio-2/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Skills from './components/Skills'
+import WorkExperience from './components/WorkExperience'
+import About from './components/About'
+import Contact from './components/Contact'
+
+
+export default function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Skills/>
+      <WorkExperience/>
+      <Contact/> 
+    </div>
+  )
+}
